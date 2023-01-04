1b983898b77b55cb77ea29c6dcc26254081760b3
Jivko Nushev <jivkon1410@gmail.com>
2023-01-04 19:08:01 +0200

    update

--- pool-JivkoNushev/main.cc
+++ pool-JivkoNushev/main.cc
@@ -24,6 +24,7 @@ public:
     float x = 0, y = 0;
     Point() {}
     Point(float x, float y): x(x), y(y) {} 
+    Point(const Point& p): x(p.x), y(p.y) {} 
 
     friend ostream& operator <<(ostream& os, const Point& p);
 };
@@ -34,31 +35,55 @@ ostream& operator<<(ostream& os, const Point& p)
     return os;
 }
 
+class Line
+{
+public:
+    Point p1, p2;
+
+    Line(){}
+    Line(Point p1, Point p2): p1(p1), p2(p2){}
+    Line(const Line& line): p1(line.p1), p2(line.p2){}
+
+};
+
 class Vector2D
 {
 
-protected:
+public:
     Point p1, p2;
 
-public:
     Vector2D(){}
     Vector2D(Point p1, Point p2): p1(p1), p2(p2){}
 
     float getLength()
     {
         float a = p2.x - p1.x;
-        float b = p2.y - p2.y;
+        float b = p2.y - p1.y;
 
         return sqrt(a*a + b*b);
     }
+
+    static Point IntersectionPoint(Point line_p1, Point line_p2, Point segment_p1, Point segment_p2) 
+    {
+        // Calculate the coefficients of the line equation.
+        float a = line_p2.y - line_p1.y;
+        float b = line_p1.x - line_p2.x;
+        float c = line_p2.x * line_p1.y - line_p1.x * line_p2.y;
+
+        // Calculate the intersection point.
+        Point intersection_p;
+        intersection_p.x = (b * (segment_p1.y - segment_p2.y) - a * (segment_p1.x - segment_p2.x)) / (2 * a * (segment_p1.y - segment_p2.y) - 2 * b * (segment_p1.x - segment_p2.x));
+        intersection_p.y = (a * (segment_p1.x - segment_p2.x) - b * (segment_p1.y - segment_p2.y)) / (2 * a * (segment_p1.x - segment_p2.x) - 2 * b * (segment_p1.y - segment_p2.y));
+        return intersection_p;
+    }
+
 };
 
 class Hit : public Vector2D
 {
-    //FIXME:
+public:
     float force = 1;
 
-public:
     Hit() {}
     Hit(Point p1, Point p2): Vector2D(p1, p2) {}
     Hit(Point p1, Point p2, float force): Vector2D(p1, p2), force(force)
@@ -74,15 +99,21 @@ public:
 class Ball
 {
     Point position;
+    Point startingPosition;
     float diameter = 0;
 
 public:
     Ball(){}
-    Ball(Point position , float diameter): position(position), diameter(diameter) {}
+    Ball(Point position , float diameter): position(position), diameter(diameter), startingPosition(position) {}
 
     Point getPosition() const
     {
         return position;
+    }
+
+    void setPosition(Point position)
+    {
+        this->position = position;
     }
 
     float getDiameter() const
@@ -90,9 +121,12 @@ public:
         return diameter;
     }
 
-    void hit(Hit h)
+    void hit(Hit& h)
     {
+        float x = h.force * (h.p2.x - h.p1.x);
+        float y = h.force * (h.p2.y - h.p1.y);
 
+        position = Point(x + position.x, y + position.y);
     }
 
     friend ostream& operator <<(ostream& os, const Ball& b);
@@ -121,6 +155,22 @@ public:
     {
         return points[0].x <= p.x && p.x <= points[2].x && points[0].y <= p.y && p.y <= points[2].y;
     }
+
+    float getWidth()
+    {
+        float a = points[1].x - points[0].x;  
+        float b = points[1].y - points[0].y;  
+
+        return sqrt(a * a + b * b);
+    }
+
+    float getHeight()
+    {
+        float a = points[4].x - points[0].x;  
+        float b = points[4].y - points[0].y;  
+
+        return sqrt(a * a + b * b);     
+    }
 };
 
 class EmptyBoard : public Rectangle
@@ -133,7 +183,6 @@ public:
         float width = Vector2D(p1, p2).getLength();
         float height = Vector2D(p1, p4).getLength();
 
-        // 1/2        
         if(width * 2 != height && height * 2 != width)
         {
             throw Exception("Invalid board coordinates");
@@ -142,7 +191,6 @@ public:
 
     EmptyBoard(Point p1, float width, float height): Rectangle(p1, width, height)
     {
-        // 1/2  
         if(width * 2 != height && height * 2 != width)
         {
             throw Exception("Invalid board coordinates");
@@ -159,24 +207,53 @@ public:
     Board() {}
     Board(Point p1, Point p2, Point p3, Point p4, Ball ball) : EmptyBoard(p1, p2, p3, p4), ball(ball)
     {
-        // FIXME: ball is thicker
-        if(!contains(ball.getPosition()))
+        Rectangle r(points[0], getWidth() - ball.getDiameter(), getHeight() - ball.getDiameter());
+        if(!r.contains(ball.getPosition()))
         {
             throw Exception("Invalid ball position");
         }
     }
     Board(Point p1, float width, float height, Ball ball) : EmptyBoard(p1, width, height), ball(ball)
     {
-        // FIXME: ball is thicker
-        if(!contains(ball.getPosition()))
+        Rectangle r(points[0], getWidth() - ball.getDiameter(), getHeight() - ball.getDiameter());
+        if(!r.contains(ball.getPosition()))
         {
             throw Exception("Invalid ball position");
         }
     }
 
-    Ball getBall()
+    Ball& getBall()
     {
         return ball;
+    }
+
+    void hit(Hit& h)
+    {
+        float new_x = ball.getPosition().x + h.force * (h.p2.x - h.p1.x);
+        float new_y = ball.getPosition().y + h.force * (h.p2.y - h.p1.y);
+
+        Point p(new_x, new_y);
+        Point last_p(h.p2);
+        Rectangle r(points[0], getWidth() - ball.getDiameter(), getHeight() - ball.getDiameter());
+
+        // TODO: check corners
+        Line lines[4] = {Line(points[0], points[1]), Line(points[0], points[4]), Line(points[1], points[3]), Line(points[4], points[3])}; 
+        Line l;
+        while(!r.contains(p))
+        {
+
+            // try to find intersecton with all sides and 
+            //if(lines[0].p1.x < p.x && p.x < lines[0].p2.x && p.y <= lines)
+            Point intersection = Vector2D::IntersectionPoint();
+
+            // TODO: check corners
+
+            Point p(new_x, new_y);
+            Point last_p(Point(new_x, new_y));
+        }
+
+
+        ball.setPosition(Point(new_x, new_y));
     }
 
     friend ostream& operator <<(ostream& os, const Board& b);
@@ -222,9 +299,21 @@ void initBoard(Board& board)
     }
 }
 
-void initHit(Hit& hit)
+void initHit(Board& board)
 {
-    
+    float x, y, size;
+    cin >> x >> y >> size;
+    Hit h(board.getBall().getPosition(), Point(x, y), size);
+    board.hit(h);
+}
+
+void printInfo(Board& board)
+{
+    char c; 
+    system("clear");
+    cout << board;
+    cout << "Press 1 + Enter to Exit\n";
+    cin >> c;
 }
 
 void printMainMenu()
@@ -244,8 +333,6 @@ int main(int argc, char const *argv[])
     try
     {
         Board board;
-        Hit hit;   
-        char c; 
 
         while(running)
         {
@@ -261,13 +348,10 @@ int main(int argc, char const *argv[])
                 /* code */
                 break;
             case 3:
-                system("clear");
-                cout << board;
-
-                cin.get(c);
+                printInfo(board);
                 break;
             case 4:
-                initHit(hit);
+                initHit(board);
                 break;
             case 5:
                 /* code */
